d075e081415ed138472374bc49613a95c85d17fc
Matt Wielbut <mwielbut@peekful.com>
2013-09-30 16:55:15 +0100

    Initial Commit

---
+++ StatusBarMessageExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StatusBarMessageExample
+//
+//  Created by Matt Wielbut on 9/30/13.
+//  Copyright (c) 2013 Matt Wielbut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StatusBarMessageExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StatusBarMessageExample
+//
+//  Created by Matt Wielbut on 9/30/13.
+//  Copyright (c) 2013 Matt Wielbut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
