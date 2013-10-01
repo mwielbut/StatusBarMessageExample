a2250e86b2ccca87dff65080d71039c75a89337d
Matt Wielbut <mwielbut@peekful.com>
2013-10-01 13:45:10 +0100

    Send screenshots and update comments

--- StatusBarMessageExample/AppDelegate.m
+++ StatusBarMessageExample/AppDelegate.m
@@ -35,13 +35,23 @@
     
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
+    // your existing root controller
     MyController *myController = [[MyController alloc] init];
+    
+    // create an instance of the status container controller, passing in your root controller
     StatusBarController * statusBarController = [[StatusBarController alloc] initWithRootViewController:myController];
+    
+    // (optional) if you need your root controller to be notified of changes to the status bar, set the delgate
     statusBarController.delegate = myController;
     
+    // (optional) pass the status bar controller into the root controller so we can pass status messages
+    // an alternative would be to use the singleton pattern or notification center which our outside the
+    // scope of this example
     myController.statusBarController = statusBarController;
     
+    // set the status bar controller as the root window controller
     [_window setRootViewController:statusBarController];
+    
     [self.window makeKeyAndVisible];
     
     return YES;
